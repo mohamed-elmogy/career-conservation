59a9e3c54bd6cbd1fdfaf9256e345b0d96601e62
mohamed-elmogy <85038174+mohamed-elmogy@users.noreply.github.com>
2026-02-05 15:18:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,13 @@ me = Me()
 
 # ===== FASTAPI APP =====
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Production لاحقاً هنقفلها
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class ChatRequest(BaseModel):
     message: str
     history: list = []
@@ -190,6 +196,7 @@ app = gr.mount_gradio_app(app, demo, path="/ui")
 # ===== RUN =====
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=7860)
+
 
 
 
